e0a054d8b7abf10517c9c81724aa24c7497f1d6f
kilometers <kilo@meters.sh>
2023-10-01 00:57:59 -0700

    React/Tailwind boilerplate

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div class="bg-red-500 p-10 text-bold">
+            Hello, world!
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.renderer.config.js
@@ -0,0 +1,15 @@
+const rules = require('./webpack.rules');
+const path = require('path')
+
+rules.push({
+    test: /\.css$/,
+    include: path.resolve(__dirname, 'src'),
+    use: ['style-loader', 'css-loader', 'postcss-loader'],
+});
+
+module.exports = {
+    // Put your normal webpack config below here
+    module: {
+        rules,
+    },
+};
